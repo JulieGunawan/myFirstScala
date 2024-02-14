c46b09e131402eb0517adbd6c08f6538459861a9
JulieGunawan <48563515+JulieGunawan@users.noreply.github.com>
2024-02-14 16:27:17 -0700

    adding f interpolation

--- src/main/scala/Demo/Demo.scala
+++ src/main/scala/Demo/Demo.scala
@@ -5,6 +5,9 @@ object Demo {
       val name="mark";
       val age = 18;
       println("Hello World! "+ name + " who is " + age + " years old ");
+      println(s"$name is $age years old");
+      //to use type interpolation
+      println(f"$name%s is $age%d years old")
     }
 
 
