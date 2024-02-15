44b7ce44cba6f404fb49678d82416f8f017c3eb8
JulieGunawan <48563515+JulieGunawan@users.noreply.github.com>
2024-02-14 20:03:01 -0700

    do while doesn't work

--- src/main/scala/Demo/Demo.scala
+++ src/main/scala/Demo/Demo.scala
@@ -3,10 +3,16 @@ package Demo
 object Demo {
     def main(args: Array[String]): Unit = {
       var x = 0;
+      var y = 0;
       while (x<10){
         println("x= "+x);
         x+=1;
         //x++; this is not allowed in scala
       }
+
+      do {
+        println("y= " + y);
+        y +=1;
+      } while (y<10);
     }
 }
